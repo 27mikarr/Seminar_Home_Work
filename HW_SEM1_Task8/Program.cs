0e0c67ccce7dfc1ae6f2c5324dbf50332486ca42
Alexander K <wrx-mikarr@ya.ru>
2023-01-19 02:26:23 +0300

    решение  четвертой  задачи

---
+++ HW_SEM1_Task8/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4  8 -> 2, 4, 6, 8
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите положительное целое  число: ");
+        int number1 = int.Parse(Console.ReadLine());
+        int count = 2;
+
+        if (number1 < 0)
+        {
+            Console.WriteLine(" Введенное число отрицательное ");
+        }
+        else if (number1 < 2)
+        {
+            Console.WriteLine(" Введенное число меньше 2  ");
+        }
+        else
+        {
+            while (count < number1)
+            {
+                Console.Write("  ");
+                Console.Write(count);
+                Console.Write("  ");
+                count = count+2;
+            }
+        }
+    }
+}
