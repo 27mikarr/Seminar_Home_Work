354a436ed839d6043ee2cf114bd22c3629ee8cc5
Alexander K <wrx-mikarr@ya.ru>
2023-01-19 01:23:50 +0300

    решение  третьей задачи

--- HW_SEM1_Task6/Program.cs
+++ HW_SEM1_Task6/Program.cs
@@ -1,8 +1,14 @@
-﻿namespace HW_SEM1_Task6;
-class Program
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да  -3 -> нет 7 -> нет
+
+Console.Write("Введите целое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+if ( number1 % 2==1 || number1 % 2==-1 )
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    Console.WriteLine($" Введенное число : {number1} нечетное ");
+}
+else
+{
+Console.WriteLine($" Введенное число : {number1} четное ");
 }
