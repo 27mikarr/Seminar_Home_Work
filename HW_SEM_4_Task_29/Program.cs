25f31cfecab91e983fa6cc6619b39db363aa1387
Alexander K <wrx-mikarr@ya.ru>
2023-01-31 02:50:46 +0300

    решение задачи 29  а

--- HW_SEM_4_Task_29/Program.cs
+++ HW_SEM_4_Task_29/Program.cs
@@ -28,7 +28,7 @@ void PrintArray(int[] array)
     Console.Write("[ ");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(array[i] + " ,");
     }
     Console.Write(" ]");
 }
