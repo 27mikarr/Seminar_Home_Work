801fd7131020ca482457371fad2e0c7a4253e94b
Alexander K <wrx-mikarr@ya.ru>
2023-01-31 02:47:10 +0300

    решение задачи 29

---
+++ HW_SEM_4_Task_29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов в заданном пользователем диапазоне и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Задайте размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+Console.Write("Далее введите нижнюю границу (включительно) численного диапазона заполнения массива: ");
+int lownum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Далее введите верхнюю границу (включительно) численного диапазона заполнения массива: ");
+int hinum = Convert.ToInt32(Console.ReadLine());
+
+FillArray(array);
+Console.Write("Массив вот такой получился ");
+PrintArray(array);
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(lownum, hinum + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write(" ]");
+}
